4295303ed68694fc3276e9ee22818cf2dc4a26e7
s-kaviyasri30 <160023528+s-kaviyasri30@users.noreply.github.com>
2024-08-11 12:23:34 +0530

    Create variable.java

---
+++ variable.java
@@ -0,0 +1,9 @@
+class hello{
+    public static void main(String arg[])
+    {
+      int container = 67;
+      String jhon ="Jhon";
+      String container2 = jhon; 
+        System.out.println(container2);
+    }
+}
